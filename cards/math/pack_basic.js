1f78032677fa0ecca064def9178c82448b178602
Yiang <yiangforwork@gmail.com>
2023-07-27 23:28:11 +0800

    Update Cards

--- cards/math/pack_basic.js
+++ cards/math/pack_basic.js
@@ -1,13 +1,15 @@
 export default {
     name: "basic",
     cards: [
+        [   "圆体积公式","V=\\frac{4}{3}\\pi r^3",
+            "圆柱体积公式","V=\\pi r^2h",
+            "圆锥体积公式","V=\\frac{1}{3}\\pi r^2h",
+        ],
         ["C^k_n=C(n,k)=?", "\\frac{n!}{(n-k)!*k!}"],
         ["双曲函数的奇偶性","\\cosh和sech是偶函数，其余是奇函数"],
         ["如何求解关于y的积分(dy)?","\\int_{a}^b 2\\pi yxdy \\\\ 当积分是关于y的时候，把y当做x \\\\ 不要把y换成关于x的表达式 \\\\ 直接代入上下限算就好了"],
         ["\\int_{-5}^{5} \\sqrt{25-x^2}dx \\\\ x = 5\\sin(\\theta), 上下限要变成什么?","\\sin(\\theta)=\\frac{x}{5} \\\\ \\theta=\\sin^{-1}(\\frac{x}{5}) \\\\ 上限从5变成\\sin^{-1}(\\frac{5}{5}) \\\\ 下限从-5变成\\sin^{-1}(\\frac{-5}{5})"],
         ["(y-b)^2+(x-a)^2=r^2代表?","代表一个圆心为(a,b) \\\\ 半径为r的圆的方程"],
-        ["text:Volume of Sphere:","V=\\frac{4}{3}\\pi r^3"],
-        ["text:Volume of Cylinder:","V=\\pi r^2h"],
         ["sin(x)的周期，cos(x)的周期 \\\\ sin(2x)的周期, |sin(x)|的周期","2\\pi, 2\\pi \\\\ \\pi, \\pi"],
         ["若f(x)以T为周期，则f(ax+b)的周期为?","\\frac{T}{|a|}"],
         ["sgn(x)是什么","符号函数, x<0, sgn(x)=-1 \\\\ x=0, sgn(x)=0 \\\\ x>0, sgn(x)=1"],
