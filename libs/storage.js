e8074407850e2d68346d81ae44fbc27e5171e473
Yiang <yiangforwork@gmail.com>
2023-08-20 13:20:22 -0400

    Add set method for storage

--- libs/storage.js
+++ libs/storage.js
@@ -2,5 +2,8 @@ export default {
     get(name, defaultValue = null) {
         const value = localStorage.getItem(name) || defaultValue;
         return value || defaultValue;
+    },
+    set(name, value) {
+        localStorage.setItem(name, value);
     }
 }
